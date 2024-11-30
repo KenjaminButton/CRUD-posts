e66fc1f44ee17d1059b3ed6a9b9fa410312cd62b
Kenjamin Button <kennethpchang@gmail.com>
2024-11-29 22:49:07 -0800

    Like and unlike posts showing user feedback in real time, albeit with a 2 second delay

--- actions/posts.js
+++ actions/posts.js
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 
 export async function createPost(prevState, formData) {
   const title = formData.get('title');
@@ -33,8 +34,6 @@ export async function createPost(prevState, formData) {
     throw new Error('Image upload failed, post was not created. Blame Cloudinary! Please try again later... Wait, to be fair, it is probably my fault for updating REACT and React-DOM.. =*( ')
   }
   
-  
-
   await storePost({
     imageUrl: imageUrl,
     title,
@@ -46,6 +45,9 @@ export async function createPost(prevState, formData) {
 }
 
 export async function togglePostLikeStatus(postId) {
-  updatePostLikeStatus(postId, 2)
+  await updatePostLikeStatus(postId, 2)
+  // revalidatePath('/feed')
+  // All pages revalidated
+  revalidatePath('/', 'layout')
 }
 
